7a28397e6b0d89723cf8f8c89f06d3c700305ef2
Joyanna Hirst <helloidevthings@gmail.com>
2022-02-21 12:44:51 -0500

    updates

--- src/pages/lab/CSSAnimations/styled.js
+++ src/pages/lab/CSSAnimations/styled.js
@@ -5,6 +5,11 @@ export default styled.main`
   height: 100%;
   background: linear-gradient(45deg, #01016a, #7026d3, #fc04fc);
   font-family: 'Gentium Book Basic', serif;
+  /* font-family: 'Fira Sans', sans-serif; */
+  /* font-family: 'Amaranth', sans-serif; */
+
+  font-weight: 400;
+
   font-size: calc(14px + (22 - 14) * ((100vw - 300px) / (2100 - 300)));
   color: #000;
   line-height: 1.45;
@@ -14,7 +19,10 @@ export default styled.main`
     font-size: 3em;
     background: #fff;
     padding: 0.1em 0.3em;
+    /* font-family: 'Amaranth', sans-serif; */
+    /* font-family: 'Secular One', sans-serif; */
   }
+
   a {
     color: #fc04fc;
   }
@@ -24,5 +32,13 @@ export default styled.main`
     padding: 1em;
     position: relative;
     box-shadow: 8px 8px 0 #fc04fc;
+  }
+  code {
+    background: linear-gradient(45deg, #7026d3, #fc04fc);
+    color: #fff;
+    padding: 0.1em 0.3em;
+  }
+  ul li {
+    line-height: 1.6;
   }
 `;
